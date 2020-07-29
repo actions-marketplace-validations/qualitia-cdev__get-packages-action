d12f1e7e8b65a83eac18aa5519dc646383228100
cloudserviceuser <46016461+cloudserviceuser@users.noreply.github.com>
2020-07-29 14:22:27 +0900

    set commit env

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,14 +9,16 @@ readonly PACKAGE_FILE=$4
 readonly TARGET_DIR=$5
 readonly package=$(yq -y .package ${PACKAGE_FILE} | head -n1)
 readonly s3_bucket="s3://${package}"
+readonly commit_id=$(echo ${package} | tr "." "\n" | head -n 1 | tr "." "\n" | tail -n 1)
 
 echo "[Show valuables]"
 echo "AWS_DEFAULT_REGION: ${AWS_DEFAULT_REGION}"
 echo "package: ${package}"
+echo "commit_id: ${commit_id}"
 echo "TARGET_DIR: ${TARGET_DIR}"
 echo "PACKAGE_FILE: ${PACKAGE_FILE}"
-
 echo "[Start copy ${s3_bucket} to current dir]"
+echo "::set-env name=COMMIT_ID::${commit_id}"
 
 aws s3 cp ${s3_bucket} package.tar.gz
 
